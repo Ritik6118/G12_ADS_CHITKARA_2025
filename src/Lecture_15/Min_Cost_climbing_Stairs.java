b8d0195adec0033cef8d1de8321ce62077c6ad60
ritik <ritik6118a@gmail.com>
2025-04-25 17:14:53 +0530

    Dynamic Programing

--- src/Lecture_15/Min_Cost_climbing_Stairs.java
+++ src/Lecture_15/Min_Cost_climbing_Stairs.java
@@ -7,4 +7,28 @@ public class Min_Cost_climbing_Stairs {
 
 	}
 
+	public int minCostClimbingStairs(int[] cost) {
+		dp = new Integer[cost.length];
+		int a = solve(0, cost);
+		dp = new Integer[cost.length];
+		int b = solve(1, cost);
+		return Math.min(a, b);
+
+	}
+
+	Integer[] dp;
+
+	private int solve(int i, int[] cost) {
+		// TODO Auto-generated method stub
+		if (i >= cost.length) {
+			return 0;
+		}
+		if (dp[i] != null) {
+			return dp[i];
+		}
+		int a = cost[i] + solve(i + 1, cost);
+		int b = cost[i] + solve(i + 2, cost);
+		return dp[i] = Math.min(a, b);
+	}
+
 }
